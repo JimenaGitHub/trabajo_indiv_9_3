95e6bba20d3ad01888c3b84a86f2cd9b14f973b4
JimenaGitHub <jaliagasanhueza@gmail.com>
2022-09-25 17:07:13 -0300

    updated

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,6 @@
 class Publication < ApplicationRecord
+  has_many :boards, dependent: :destroy
+  has_many :users, through: :boards
   belongs_to :tag
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :boards
   resources :publications
   devise_for :users
   resources :tags
